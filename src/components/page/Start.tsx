e49e70c99a375d989a050841a29e4b96915995dc
Shieru093 <shieru093@gmail.com>
2023-12-27 21:38:10 +0900

    スタート画面の実装、フォントの設定

--- src/components/page/Start.tsx
+++ src/components/page/Start.tsx
@@ -1,11 +1,17 @@
-import LinkButton from '@/components/ui/Button/LinkButton';
+import { StartPageDateTime } from '@/components/ui/DateTime';
+import Link from 'next/link';
+
 export default function Start() {
 	return (
-		<div>
-			<LinkButton
-				text="start"
-				link={'/calendar'}
-			/>
+		<div className="bg-gradient-to-bl  flex flex-col from-gray-900 items-center justify-center min-h-screen to-slate-700">
+			<h1 className="pb-5 text-5xl text-gray-50">シンプルカレンダー</h1>
+			<StartPageDateTime />
+			<Link
+				href="/calendar"
+				className="bg-blue-700 px-3 py-1 rounded-xl shadow-xl text-3xl text-gray-50 hover:bg-blue-500"
+			>
+				スタート
+			</Link>
 		</div>
 	);
 }
